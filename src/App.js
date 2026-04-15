2100fb3746a0981475d5345602915ff060dbba04
Samour chandra paul <scpsamour@gmail.com>
2026-04-16 00:05:43 +0600

    Remove Props
    Remove props and add  context Api

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
 
         <Header />
         <Main />
-        <Archive onAddPost={handleAddPost} />
+        <Archive />
         <Footer />
       </section>
     </PostContext.Provider>
@@ -107,8 +107,8 @@ function Main() {
   const { posts, onAddPost } = useContext(PostContext);
   return (
     <main>
-      <FormAddPost/>
-      <Posts/>
+      <FormAddPost />
+      <Posts />
     </main>
   );
 }
@@ -123,7 +123,7 @@ function Posts() {
 }
 
 function FormAddPost() {
-   const { onAddPost } = useContext(PostContext);
+  const { onAddPost } = useContext(PostContext);
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
@@ -165,7 +165,8 @@ function List({ posts }) {
   );
 }
 
-function Archive({ onAddPost }) {
+function Archive() {
+  const { onAddPost } = useContext(PostContext)
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
